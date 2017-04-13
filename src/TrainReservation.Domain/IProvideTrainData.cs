5f6d0f88c2b7e05b831edbabce49c03de4a85803
Thomas PIERRAIN <thomas@pierrain.net>
2017-04-14 01:05:33 +0200

    Add second failing test

--- src/TrainReservation.Domain/IProvideTrainData.cs
+++ src/TrainReservation.Domain/IProvideTrainData.cs
@@ -7,6 +7,7 @@ namespace TrainReservation.Domain
     public interface IProvideTrainData
     {
         List<SeatWithBookingReference> GetSeats(string trainId);
+        void MarkSeatsAsReserved(string trainId, BookingReference bookingReference, List<Seat> seats);
     }
 
     public class SeatWithBookingReference
--- tests/TrainReservation.Tests/TicketOfficeTests.cs
+++ tests/TrainReservation.Tests/TicketOfficeTests.cs
@@ -14,7 +14,7 @@ namespace TrainReservation.Tests
         public void Should_reserve_seats_when_unreserved_seats_are_available()
         {
             // setup mocks
-            string expectedBookingId = "75bcd15";
+            var expectedBookingId = "75bcd15";
             var bookingReferenceProvider = Substitute.For<IProvideBookingReferences>();
             bookingReferenceProvider.GetBookingReference().Returns(expectedBookingId);
 
@@ -29,6 +29,29 @@ namespace TrainReservation.Tests
             Check.That(reservation.TrainId).IsEqualTo(trainId);
             Check.That(reservation.BookingId).IsEqualTo(expectedBookingId);
             Check.That(reservation.Seats).ContainsExactly(new Seat("A", 1), new Seat("A", 2), new Seat("A", 3));
+        }
+
+        [Test]
+        public void Should_mark_seats_as_reserved_once_reserved()
+        {
+            // setup mocks
+            var expectedBookingId = "75bcd15";
+            var bookingReferenceProvider = Substitute.For<IProvideBookingReferences>();
+            bookingReferenceProvider.GetBookingReference().Returns(expectedBookingId);
+
+            var trainId = "express_2000";
+            var trainDataProvider = Substitute.For<IProvideTrainData>();
+            trainDataProvider.GetSeats(trainId).Returns(new List<SeatWithBookingReference>() { new SeatWithBookingReference(new Seat("A", 1), new BookingReference("34Dsq")), new SeatWithBookingReference(new Seat("A", 2), BookingReference.Null), new SeatWithBookingReference(new Seat("A", 3), new BookingReference("34Dsq")) });
+
+            // act
+            var ticketOffice = new TicketOffice(bookingReferenceProvider, trainDataProvider);
+            var reservation = ticketOffice.MakeReservation(new ReservationRequest(trainId, 1));
+
+            Check.That(reservation.TrainId).IsEqualTo(trainId);
+            Check.That(reservation.BookingId).IsEqualTo(expectedBookingId);
+            Check.That(reservation.Seats).ContainsExactly(new Seat("A", 2));
+
+            trainDataProvider.Received().MarkSeatsAsReserved(trainId, new BookingReference(reservation.BookingId), new List<Seat>(){new Seat("A", 2)});
         }
     }
 }
