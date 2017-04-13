51c6bb35a5465f46a4eebda4879ef28d7ab986b7
Thomas PIERRAIN <thomas@pierrain.net>
2017-04-14 00:02:36 +0200

    Add first test

--- src/TrainReservation.Domain/Reservation.cs
+++ src/TrainReservation.Domain/Reservation.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
+using KataTrainReservation;
 
-namespace KataTrainReservation
+namespace TrainReservation
 {
     public class Reservation
     {
---
+++ tests/TrainReservation.Tests/TicketOfficeTests.cs
@@ -0,0 +1,69 @@
+﻿using KataTrainReservation;
+using NFluent;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace TrainReservation.Tests
+{
+    [TestFixture]
+    public class TicketOfficeTests
+    {
+        [Test]
+        public void Should_reserve_seats_when_unreserved_seats_are_available()
+        {
+            var bookingReferenceProvider = Substitute.For<IProvideBookingReferences>();
+            string expectedBookingId = "75bcd15";
+            bookingReferenceProvider.GetBookingReference().Returns(expectedBookingId);
+
+            var trainDataProvider = new TrainDataProvider();
+
+            var ticketOffice = new TicketOffice(bookingReferenceProvider, trainDataProvider);
+            var trainId = "express_2000";
+
+            var reservationRequest = new ReservationRequest(trainId, 3);
+            var reservation = ticketOffice.Reserve(reservationRequest);
+
+            Check.That(reservation.TrainId).IsEqualTo(trainId);
+            Check.That(reservation.BookingId).IsEqualTo(expectedBookingId);
+            Check.That(reservation.Seats).ContainsExactly(new Seat("A", 1), new Seat("A", 2), new Seat("A", 3));
+        }
+    }
+
+    public interface IProvideTrainData
+    {
+    }
+
+    public class TrainDataProvider : IProvideTrainData
+    {
+    }
+
+    public interface IProvideBookingReferences
+    {
+        string GetBookingReference();
+    }
+
+    public class BookingReferenceProvider : IProvideBookingReferences
+    {
+        public string GetBookingReference()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class TicketOffice
+    {
+        private IProvideBookingReferences bookingReferenceProvider;
+        private IProvideTrainData trainDataProvider;
+
+        public TicketOffice(IProvideBookingReferences bookingReferenceProvider, IProvideTrainData trainDataProvider)
+        {
+            this.bookingReferenceProvider = bookingReferenceProvider;
+            this.trainDataProvider = trainDataProvider;
+        }
+
+        public Reservation Reserve(ReservationRequest reservationRequest)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
